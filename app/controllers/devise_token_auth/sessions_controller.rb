f27b5724ce030fe8c2a086e75ce074aa39835838
rafael <skcc321@gmail.com>
2017-02-27 15:25:00 +0200

    pusher token

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -37,6 +37,7 @@ module DeviseTokenAuth
         # create client id
         @client_id = SecureRandom.urlsafe_base64(nil, false)
         @token     = SecureRandom.urlsafe_base64(nil, false)
+        @resource.pusher_token = SecureRandom.urlsafe_base64(nil, false)
 
         @resource.tokens[@client_id] = {
           token: BCrypt::Password.create(@token),
